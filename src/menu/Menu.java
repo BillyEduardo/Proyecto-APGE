f58a281c2053619d3594bf64d9ced72d8edff589
Billi <Billi@DESKTOP-5OP8JEJ>
2023-11-22 22:49:06 -0400

    Formulario 1

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -3,10 +3,17 @@ package menu;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Menu {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +45,39 @@ public class Menu {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Log in");
+		btnNewButton.setBounds(173, 156, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Sign in ");
+		btnNewButton_1.setBounds(173, 190, 89, 23);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		textField = new JTextField();
+		textField.setBounds(151, 125, 125, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(151, 81, 125, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Password");
+		lblNewLabel.setForeground(new Color(192, 192, 192));
+		lblNewLabel.setBounds(151, 112, 65, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("User");
+		lblNewLabel_1.setForeground(new Color(192, 192, 192));
+		lblNewLabel_1.setBounds(155, 66, 89, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("APGE");
+		lblNewLabel_2.setFont(new Font("Microsoft JhengHei", Font.PLAIN, 18));
+		lblNewLabel_2.setBounds(184, 11, 53, 23);
+		frame.getContentPane().add(lblNewLabel_2);
 	}
-
 }
